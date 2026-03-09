ac10b57128b467712af7416217dc05752d63b5fc
sakib-hasan-11 <sakib.sosbangladesh@gmail.com>
2026-03-09 15:44:51 +0600

    cd pipeline bug fixed-5

--- src/scripts/load_model.py
+++ src/scripts/load_model.py
@@ -1,38 +1,42 @@
-from langchain_openai import OpenAIEmbeddings,ChatOpenAI
-import os 
+import os
+
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
 from utils.log import get_logger
 
 logger = get_logger(__name__)
 
 
-
-def load_embed_model(model_name='text-embedding-3-small'):
+def load_embed_model(model_name="text-embedding-3-small"):
+    model = None
     try:
         model = OpenAIEmbeddings(
-            api_key=os.environ['OPENAI_API_KEY'],
-            model = 'text-embedding-3-small'
+            api_key=os.environ["OPENAI_API_KEY"], model="text-embedding-3-small"
         )
+    except KeyError as e:
+        logger.info(f"embed model not loaeded..\n {e}")
+        raise
     except Exception as e:
-        logger.info(f'embed model not loaeded..\n {e}')
+        logger.info(f"embed model not loaeded..\n {e}")
+        return None
 
     return model
 
 
-
-
-
-
-
-def load_llm(model_name='gpt-4o-mini'):
+def load_llm(model_name="gpt-4o-mini"):
+    model = None
     try:
         model = ChatOpenAI(
             model=model_name,
-            api_key=os.environ['OPENAI_API_KEY'],
+            api_key=os.environ["OPENAI_API_KEY"],
             temperature=1,
-            max_completion_tokens=500
+            max_completion_tokens=500,
         )
-    except Exception as e : 
-        logger.info(f'llm not loaded... {e}')
+    except KeyError as e:
+        logger.info(f"llm not loaded... {e}")
+        raise
+    except Exception as e:
+        logger.info(f"llm not loaded... {e}")
+        return None
 
     return model
-
